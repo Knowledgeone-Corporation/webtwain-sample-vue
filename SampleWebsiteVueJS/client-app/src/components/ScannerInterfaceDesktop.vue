0104ec57c025529ab066b0d241a01bf13ad53465
tinhngo-saigontechnology <107822434+tinhngosaigontechnology@users.noreply.github.com>
2025-04-29 12:08:08 +0700

    feat: update sample code to version 3.0.0 (#10)
    * feat: update sample code to version 3.0.0
    
    * feat: update sample code to version 3.0.0 phase 2

--- SampleWebsiteVueJS/client-app/src/components/ScannerInterfaceDesktop.vue
+++ SampleWebsiteVueJS/client-app/src/components/ScannerInterfaceDesktop.vue
@@ -37,9 +37,6 @@
                     <input v-model="outputFilename" id="sel-output-name" class="form-control" type="text"
                         placeholder="Please enter a file name" />
                     <label class="scanning-label mt-2">Output File Type</label>
-                    <span class="filetype-restriction" v-if="isDisplayFileRestriction">
-                        File types restricted for multiple page scans
-                    </span>
                     <select v-model="selectedFileTypeOption" class="form-control"
                         @change="handleFileTypeChange($event.target.value)">
                         <option v-for="option in fileTypeOptions" v-bind:key="option.value" v-bind:value="option.value">
@@ -83,7 +80,7 @@ import $ from 'jquery'
 import { K1WebTwain } from '../lib/k1scanservice/js/k1ss.js'
 import {
     convertRawOptions, generateScanFileName, saveDefaultScanSettings,
-    getDefaultScanSettings, renderOptions
+    getDefaultScanSettings, getScannerDetails, renderOptions
 } from '../utils/scanningUtils.js'
 
 export default {
@@ -101,7 +98,6 @@ export default {
             isDisplayScanningSection: false,
             isDisableScanButton: true,
             isDisableFinalizeSection: true,
-            isDisplayFileRestriction: false,
             isDisplayOCR: false
         }
     },
@@ -109,11 +105,14 @@ export default {
         handleDeviceChange: function (deviceId) {
             this.isDisableScanButton = parseInt(deviceId) === -1;
 
-            let defaultSettings = getDefaultScanSettings();
+            const defaultSettings = getDefaultScanSettings();
+            let scannerDetails = getScannerDetails(defaultSettings);
+            scannerDetails.ScanSource = parseInt(deviceId);
+
             saveDefaultScanSettings(
                 defaultSettings?.ScanType ?? this.selectedFileTypeOption,
                 defaultSettings?.OCRType ?? this.selectedOcrOption,
-                deviceId
+                scannerDetails
             );
         },
         acquire: function () {
@@ -122,33 +121,7 @@ export default {
             };
 
             K1WebTwain.StartScan(acquireRequest)
-                .then(response => {
-                    if (response.pageCount > 1) {
-                        this.isDisplayFileRestriction = true;
-                        let fileType = this.selectedFileTypeOption;
-                        if (
-                            fileType === "JPG" ||
-                            fileType === "GIF" ||
-                            fileType === "PNG" ||
-                            fileType === "BMP"
-                        ) {
-                            this.selectedFileTypeOption = K1WebTwain.Options.OutputFiletype.TIFF;
-                        }
-                        this.fileTypeOptions = this.fileTypeOptions.filter(
-                            (fileType) =>
-                                fileType.value === "PDF" ||
-                                fileType.value === "PDF/A" ||
-                                fileType.value === "TIF"
-                        );
-                    } else {
-                        this.isDisplayFileRestriction = false;
-                        let mappedFileTypeOptions = convertRawOptions(
-                            K1WebTwain.Options.OutputFiletype,
-                            true
-                        );
-                        this.fileTypeOptions = renderOptions(mappedFileTypeOptions);
-                    }
-
+                .then(() => {
                     this.isDisableFinalizeSection = false;
                     this.isDisableScanButton = true;
                 })
@@ -168,14 +141,16 @@ export default {
                 this.outputFilename = generateScanFileName();
                 this.isDisplayScanningSection = true;
 
-                let scanSettings = getDefaultScanSettings();
-                if (scanSettings) {
-                    this.selectedDeviceId = scanSettings.ScanSource;
+                const scanSettings = getDefaultScanSettings();
+                const deviceId = scanSettings?.ScannerDetails?.ScanSource;
+
+                if (deviceId) {
+                    this.selectedDeviceId = deviceId;
                     this.selectedFileTypeOption = scanSettings.ScanType;
                     this.selectedOcrOption = scanSettings.UseOCR
                         ? scanSettings.OCRType
                         : K1WebTwain.Options.OcrType.None;
-                    this.isDisableScanButton = parseInt(scanSettings.ScanSource) === -1;
+                    this.isDisableScanButton = parseInt(deviceId) === -1;
                 }
 
                 this.isDisplayOCR =
@@ -192,20 +167,20 @@ export default {
             this.isDisplayOCR =
                 outputType === K1WebTwain.Options.OutputFiletype.PDF ||
                 outputType === K1WebTwain.Options.OutputFiletype["PDF/A"];
-            let defaultSettings = getDefaultScanSettings();
+            const defaultSettings = getDefaultScanSettings();
+
             saveDefaultScanSettings(
                 outputType,
-                defaultSettings?.OCRType ?? this.selectedOcrOption,
-                defaultSettings?.ScanSource ?? this.selectedDeviceId
+                defaultSettings?.OCRType ?? this.selectedOcrOption
             );
         },
         handlOcrTypeChange: function (ocrType) {
             this.selectedOcrOption = ocrType
-            let defaultSettings = getDefaultScanSettings();
+            const defaultSettings = getDefaultScanSettings();
+
             saveDefaultScanSettings(
                 defaultSettings?.ScanType ?? this.selectedFileTypeOption,
-                ocrType,
-                defaultSettings?.ScanSource ?? this.selectedDeviceId
+                ocrType
             );
         },
         handleCancelFinalization: function () {
